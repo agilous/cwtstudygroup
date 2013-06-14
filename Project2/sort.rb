9be814c6c63dc1f124cfdaf2f1957aadc232c5fa
Bill Barnett <bill@gaslight.co>
2013-06-14 10:12:31 -0400

    Remove a bit of senseless repetition.

--- Project2/sort.rb
+++ Project2/sort.rb
@@ -11,10 +11,6 @@ def pretty_print(name, result)
   puts "#{name}\n  Before: #{result}\n  After: #{funny_sort(result)}\n"
 end
 
-example1 = ["qwerty123", "abc99", "no_numbers", "-100X500"]
-example2 = ["June 2013", "13 Feb 2005", "1961/06/08", "b. 1971"]
-example3 = ["Perl 6", "Ruby 1.9.3", "PHP 5.4.6", "jre-7u21-linux-x64.rpm"]
-
-pretty_print("Example 1", example1)
-pretty_print("Example 2", example2)
-pretty_print("Example 3", example3)
+pretty_print("Example 1", ["qwerty123", "abc99", "no_numbers", "-100X500"])
+pretty_print("Example 2", ["June 2013", "13 Feb 2005", "1961/06/08", "b. 1971"])
+pretty_print("Example 3", ["Perl 6", "Ruby 1.9.3", "PHP 5.4.6", "jre-7u21-linux-x64.rpm"])
